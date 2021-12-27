35bf39272452a82a35936d378a5e462d6fc7b0cc
Sergey <zdeneke@yandex.ru>
2021-12-27 08:49:35 +0300

    chenge icon size

--- lib/src/data_table_2.dart
+++ lib/src/data_table_2.dart
@@ -852,7 +852,7 @@ class _SortArrowState extends State<_SortArrow> with TickerProviderStateMixin {
   }
 
   static const double _arrowIconBaselineOffset = -1.5;
-  static const double _arrowIconSize = 16.0;
+  static const double _arrowIconSize = 20.0;
 
   @override
   Widget build(BuildContext context) {
